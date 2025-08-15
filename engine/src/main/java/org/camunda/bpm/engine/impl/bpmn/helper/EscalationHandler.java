3d86644a9d6eb64bd02505eefb633dcacc6aef05
Anders Eide <anderse@stacc.com>
2025-08-15 09:17:30 +0000

    chore(bpmn-engine): Refactor updating of escalation data variable

--- engine/src/main/java/org/camunda/bpm/engine/impl/bpmn/helper/EscalationHandler.java
+++ engine/src/main/java/org/camunda/bpm/engine/impl/bpmn/helper/EscalationHandler.java
@@ -32,7 +32,7 @@ import org.camunda.bpm.engine.impl.tree.OutputVariablesPropagator;
 import org.camunda.bpm.engine.impl.tree.ReferenceWalker;
 import org.camunda.bpm.engine.variable.value.TypedValue;
 
-import java.util.ArrayList;
+import java.util.ArrayDeque;
 
 /**
  * Helper class handling the propagation of escalation.
@@ -119,28 +119,9 @@ public class EscalationHandler {
             + ". Cannot properly propagate escalation data for interrupting boundary event.");
       }
 
-      TypedValue value = flowScopeExecution.getVariableLocalTyped(ESCALATION_DATA_VARIABLE + "_" + activityId);
-      ArrayList<String> escalationDataList = null;
-      if (value == null) {
-        escalationDataList = new ArrayList<>();
-      } else {
-        escalationDataList = (ArrayList<String>) value.getValue();
-      }
-
-      escalationDataList.add(escalationDataString);
-      flowScopeExecution.setVariableLocal(ESCALATION_DATA_VARIABLE + "_" + activityId, escalationDataList);
+      updateEscalationDataVariable(flowScopeExecution, activityId, escalationDataString);
     } else {
-      TypedValue value = escalationExecution.getVariableLocalTyped(ESCALATION_DATA_VARIABLE + "_" + activityId);
-      ArrayList<String> escalationDataList = null;
-      if (value == null) {
-        escalationDataList = new ArrayList<>();
-      } else {
-        escalationDataList = (ArrayList<String>) value.getValue();
-      }
-
-
-      escalationDataList.add(escalationDataString);
-      escalationExecution.setVariableLocal(ESCALATION_DATA_VARIABLE + "_" + activityId, escalationDataList);
+      updateEscalationDataVariable(escalationExecution, activityId, escalationDataString);
     }
 
     escalationExecution.executeActivity(escalationHandler);
@@ -155,4 +136,17 @@ public class EscalationHandler {
     }
   }
 
+  private static void updateEscalationDataVariable(ActivityExecution execution, String activityId, String escalationDataString) {
+    TypedValue value = execution.getVariableLocalTyped(ESCALATION_DATA_VARIABLE + "_" + activityId);
+    ArrayDeque<String> escalationDataList;
+    if (value == null) {
+      escalationDataList = new ArrayDeque<>();
+    } else {
+      //noinspection unchecked
+      escalationDataList = (ArrayDeque<String>) value.getValue();
+    }
+
+    escalationDataList.add(escalationDataString);
+    execution.setVariableLocal(ESCALATION_DATA_VARIABLE + "_" + activityId, escalationDataList);
+  }
 }
